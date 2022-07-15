5c65081db2d08e939a9918cd41657b6486ff4f1b
Md. Sadman Siraj <mdsadmansiraj@gmail.com>
2022-07-15 16:51:22 -0600

    Add files via upload

---
+++ requestorguest.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <curl/curl.h>
+
+#define OK 0
+#define INIT_ERR 1
+#define REQ_ERR 2
+
+#define URL "http://10.0.2.2:8000"
+
+int main(void) {
+	CURL *curl;
+	CURLcode res;
+
+	curl = curl_easy_init();
+	if (curl) {
+		curl_easy_setopt(curl, CURLOPT_URL, URL);
+		curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+		res = curl_easy_perform(curl);
+		if(res != CURLM_OK) {
+			return REQ_ERR;
+			}
+		curl_easy_cleanup(curl);
+		} else {
+		return INIT_ERR;
+		}
+	return OK;
+}
